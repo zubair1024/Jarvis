7b3f8a639f821a8378bd7e0cf129bc4e53da88e9
Zubair Ahmed <zubair1024@gmail.com>
2013-09-01 19:46:18 +0530

    0.0.1

---
+++ Jarvis/Form1.vb
@@ -0,0 +1,66 @@
+﻿Imports System.Speech
+
+Public Class Form1
+
+    Dim WithEvents reco As New Recognition.SpeechRecognitionEngine
+
+    Private Sub SetColor(ByVal color As System.Drawing.Color)
+
+        Dim synth As New Synthesis.SpeechSynthesizer
+
+        synth.SpeakAsync("setting the back color to " + color.ToString)
+
+        Me.BackColor = color
+
+    End Sub
+
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        reco.SetInputToDefaultAudioDevice()
+
+        Dim gram As New Recognition.SrgsGrammar.SrgsDocument
+
+        Dim colorRule As New Recognition.SrgsGrammar.SrgsRule("color")
+
+        Dim colorsList As New Recognition.SrgsGrammar.SrgsOneOf("red", "green", "blue")
+
+        colorRule.Add(colorsList)
+
+        gram.Rules.Add(colorRule)
+
+        gram.Root = colorRule
+
+        reco.LoadGrammar(New Recognition.Grammar(gram))
+
+        reco.RecognizeAsync()
+
+    End Sub
+
+    Private Sub reco_RecognizeCompleted(ByVal sender As Object, ByVal e As System.Speech.Recognition.RecognizeCompletedEventArgs) Handles reco.RecognizeCompleted
+
+        reco.RecognizeAsync()
+
+    End Sub
+
+    Private Sub reco_SpeechRecognized(ByVal sender As Object, ByVal e As System.Speech.Recognition.RecognitionEventArgs) Handles reco.SpeechRecognized
+
+        Select Case e.Result.Text
+
+            Case "red"
+
+                SetColor(Color.Red)
+
+            Case "green"
+
+                SetColor(Color.Green)
+
+            Case "blue"
+
+                SetColor(Color.Blue)
+
+        End Select
+
+    End Sub
+
+End Class
---
+++ Jarvis/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Jarvis")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("Jarvis")> 
+<Assembly: AssemblyCopyright("Copyright ©  2013")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("edc72371-9fcd-4da8-a69e-58427e19df62")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
